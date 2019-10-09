accddcf24ce7f7ea5867615d37e8f043b36d81eb
HARIAN <harian.elyoth@gmail.com>
2019-10-09 22:21:22 +0200

    ex3 question2

--- TP1/ex3-single.c
+++ TP1/ex3-single.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <time.h>
@@ -9,29 +10,29 @@
 int main(int argc, char const *argv[]) {
   char *argv1[6];
   const char *fichier = argv[1];
-  switch(fork()){
-    case -1 :
-      printf("Erreur\n");
-      break;
-    case 0 :
-      printf("Fichier : %s\n", fichier);
-      argv1[0] = "convert";
-      argv1[1] = fichier;
-      argv1[2] = "-radial-blur";
-      argv1[3] = "10";
-      char *NouveauNomFichier = "Modified-"; 
-      printf("NouveauNom1 : %s\n", NouveauNomFichier);
-      strcat(NouveauNomFichier, fichier);//Ce code c'est de la merde
-      printf("NouveauNom2 : %s\n", NouveauNomFichier);
+  const char *mod = "Modified-";
 
-      argv1[4] = NouveauNomFichier;
-      argv1[5] = NULL;
-      execv("/usr/bin/convert", argv1);
-      break;
-    default :
-      printf("Je suis le papa \n");
-      wait(2);
-      break;
+  printf("Fichier : %s\n", fichier);
+  argv1[0] = "convert";
+  argv1[1] = fichier;
+  argv1[2] = "-radial-blur";
+  argv1[3] = "10";
+  int l = strlen(fichier) + strlen(mod);
+  char *NouveauNomFichier = malloc(sizeof(char)*l); 
+  printf("NouveauNom1 : %s\n", mod);
+  for(int i = 0 ; i < l ; i++){
+    if (i < strlen(mod)){
+      NouveauNomFichier[i] = mod[i];
+    } 
+    else
+      NouveauNomFichier[i] = fichier[i-strlen(mod)];  
   }
+  printf("NouveauNom2 : %s\n", NouveauNomFichier);
+
+  argv1[4] = NouveauNomFichier;
+  argv1[5] = NULL;
+  execv("/usr/bin/convert", argv1);
+
+
   return 0;
 }
